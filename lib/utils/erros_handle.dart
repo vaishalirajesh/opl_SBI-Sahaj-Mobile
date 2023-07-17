787df5e8148d7889586a8800f3e68debefc64111
vaishalirajesh <vaishali.patel@onlinepsbloans.com>
2023-07-17 14:47:40 +0530

    Vaishali_17_07_2023 401 error handle

--- lib/utils/erros_handle.dart
+++ lib/utils/erros_handle.dart
@@ -1,16 +1,20 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:gstmobileservices/model/responsemodel/error/service_error.dart';
 import 'package:gstmobileservices/util/tg_view.dart';
+import 'package:sbi_sahay_1_0/registration/mobile/login/login.dart';
 
 void handleServiceFailError(BuildContext context, ServiceError? serviceError) {
-  if (serviceError != null ) {
-
-    if(serviceError.status==401){
-
-    }else if(serviceError.status==500){
-
-    }else{
-      TGView.showSnackBar(context: context, message: serviceError.message??"");
+  if (serviceError != null) {
+    if (serviceError.status == 401) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) => const LoginWithMobileNumber(),
+        ),
+        (route) => false, //if you want to disable back feature set to false
+      );
+    } else {
+      TGView.showSnackBar(context: context, message: serviceError.message ?? "");
     }
   }
 }
